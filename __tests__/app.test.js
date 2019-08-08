48010ab789d1acbb295f171019a66dadff523d2c
bjwine0 <bradsmilek@gmail.com>
2019-08-08 00:35:54 -0700

    some tests?

---
+++ __tests__/app.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe('app test', () => {
+    it('works?', () => {
+      expect(true).toBeTruthy();
+    })
+
+    it('can read a file', () => {
+      expect(true).toBeTruthy();
+    })
+
+    it('can save to file', () => {
+      expect(true).toBeTruthy();
+    })
+
+    it('uppercase works', () => {
+      expect(true).toBeTruthy();
+    })
+
+    fs.mockfile('')
+})
+
+// Testing
+// Write tests around all of your units
+// File Read, File Save, Uppercase
+// Mock the fs module methods so that your tests don’t use actual files
+// Test event handlers (not events themselves)
+// Use spies to help testing your logger methods (assert that console.log was called right)
